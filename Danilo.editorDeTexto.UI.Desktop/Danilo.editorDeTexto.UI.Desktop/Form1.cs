381730d618200899db98a576523b8c1651ea22c4
Daanilo <Daanilo@users.noreply.github.com>
2016-03-04 11:00:37 -0300

    Update Form1.cs

--- Danilo.editorDeTexto.UI.Desktop/Danilo.editorDeTexto.UI.Desktop/Form1.cs
+++ Danilo.editorDeTexto.UI.Desktop/Danilo.editorDeTexto.UI.Desktop/Form1.cs
@@ -19,12 +19,12 @@ namespace Danilo.editorDeTexto.UI.Desktop
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-
+            
         }
 
         private void toolStripButton9_Click(object sender, EventArgs e)
         {
-
+            close();
         }
 
         private void sairToolStripMenuItem_Click(object sender, EventArgs e)
